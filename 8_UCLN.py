cb339883f0f08fe4a8a99874014942a936b36063
DONG2209 <126535119+DONG2209@users.noreply.github.com>
2023-03-07 16:46:12 +0700

    Update 8_UCLN.py

--- 8_UCLN.py
+++ 8_UCLN.py
@@ -3,15 +3,14 @@ import math
 a=int(input('nhap a :'))
 b=int(input('nhap b :'))
 def ucln(a,b):
-    if b==0:
-        d=a
+    if a==0 or b==0:
+        return -1
     else:
         while(b>0):
             q=a//b
             r=a-q*b
             a=b;b=r
-        d=a
-    return d
+    return a
 
 print('gcd=',ucln(a,b))    
 
